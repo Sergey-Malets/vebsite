9b2d50523412db7b059d23d0b2bc3eb03ffe0716
Malets_Sergey <maletssergey@gmail.com>
2022-08-10 15:00:06 +0300

    HW_forms_2

--- main/forms.py
+++ main/forms.py
@@ -1,5 +1,5 @@
 from .models import Task_2
-from django.forms import ModelForm
+from django.forms import ModelForm, widgets, TextInput
 from .models import Comment
 
 
@@ -7,8 +7,18 @@ class Task_2_Form(ModelForm):
     class Meta:
         model = Task_2
         fields = ["title", "task", "float_field"]
+        widgets = {
+            'title': TextInput(attrs={'class':'form-control', "placeholder":"Введите название"}),
+            'task': TextInput(attrs={'class':'form-control', "placeholder":"Введите описание"}),
+            'float_field': TextInput(attrs={'class':'form-control','placeholder':"Введите рейтинг(0-10, где 10 наивысший)"})
+        }
 
 class CommentForm(ModelForm):
     class Meta:
         model = Comment
         fields = ["name", "message"]
+        widgets = {
+            'name': TextInput(attrs={'class': 'form-control', "placeholder": "Enter name"}),
+            'message': TextInput(attrs={'class': 'form-control', "placeholder": "Enter message"}),
+
+        }
