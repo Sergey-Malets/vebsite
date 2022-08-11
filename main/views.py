9567091a5f39866540522196a5f417dabc65c422
Malets_Sergey <maletssergey@gmail.com>
2022-08-11 19:33:40 +0300

    Done

--- main/views.py
+++ main/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Task_2
 from .forms import Task_2_Form
@@ -21,11 +21,20 @@ def new_page(request):
     context = {
         'form': form
     }
-    return render(request, "main/new_page.html", {'title':"Новая страница", "comments":comments})
+    return render(request, "main/new_page.html", context)#, {'title':"Новая страница", "comments":comments})
 
 def create(request):
+    error = ''
+    if request.method == 'POST':
+        form = Task_2_Form(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+        else:
+            error = 'форма была не верной'
     form = Task_2_Form()
     context = {
-        'form':form
+        'form':form,
+        'error':error
     }
     return render(request, "main/create.html", context)
